b697a9676c9f35722a5e1cf8ccfa3675139dadbd
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-03-14 10:13:08 +0300

    modified what we do section

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log
+$("document").ready(function(){
+    $(".des").click(function(){
+        $("#part1").toggle();  
+        $(".des img").toggle();
+    })
+
+    $(".dev").click(function(){
+        $("#part2").toggle();
+        $(".dev img").toggle();
+    })
+
+    $(".pro").click(function(){
+        $("#part3").toggle();
+        $(".pro img").toggle();
+    })
+})
+
