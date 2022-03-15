59639375f1d9652325386ff3a757ce013ae048a1
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-03-15 08:50:26 +0300

    added some code

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ $("document").ready(function(){
     })
 })
 
+
+
+$("form#form").on('submit',function(event){
+    event.preventDefault();
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+    let message = $("textarea#message").val();
+
+    if ($("input#name").val() && $("input#email").val()){
+        alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you... have a great day!");
+    }
+    else {
+        alert("Please provide your correct name and email!");
+       
+
+    }
+
+});
